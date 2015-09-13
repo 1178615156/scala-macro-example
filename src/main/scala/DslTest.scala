a105bcc1dd4a9b27df838730e71344ce03da89b4
1178615156 <a1178615156@gmail.com>
2015-09-13 22:29:29 +0800

    dsl

---
+++ src/main/scala/DslTest.scala
@@ -0,0 +1,38 @@
+/**
+ * Created by YuJieShui on 2015/9/13.
+ */
+object DslTest extends App {
+
+  sealed class ConnectionWord
+
+  case class ConnectionWordAnd() extends ConnectionWord
+
+  case class Human(name: String, private val s: String = "") {
+
+    def meet(human: Human) = this copy (s = this.s + s"meet ${human.name} ")
+
+    def like(human: Human) = this copy (s = this.s + s"like ${human.name} ")
+
+    def miss(human: Human) = this copy (s = this.s + s"miss ${human.name} ")
+
+    def say = s"${this.name} $s"
+
+    def and(connectionWord: ConnectionWordAnd = new ConnectionWordAnd) = this.copy(s = this.s + "and ")
+  }
+
+  val & : ConnectionWordAnd = new ConnectionWordAnd
+
+  val i = new Human("I")
+
+  val you = new Human("you")
+
+  val a =
+    i meet you and
+      () like you and
+      & miss you
+  println(a.say)
+
+  i meet you and() like you and & like you
+  i.meet(you).and().like(you).and(&).like(you)
+
+}
