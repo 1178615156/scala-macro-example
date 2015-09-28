34ed4dc0d08fc0fbae2643cf63ff8a74c8601a25
1178615156 <a1178615156@gmail.com>
2015-09-28 09:21:34 +0800

    full name

--- src/main/scala/macross/annotation/FullNameUsing.scala
+++ src/main/scala/macross/annotation/FullNameUsing.scala
@@ -6,7 +6,7 @@ import macross.annotation.FullNameMacro
  * Created by yu jie shui on 2015/9/14 15:42.
  */
 @FullNameMacro
-class FullNameUsingEntity
+case class FullNameUsingEntity()
 object FullNameUsing extends App{
   val fn=new FullNameUsingEntity().fullName
   assert(fn=="macros.annotation.FullNameUsingEntity")
