f861c33f840a5a1692cf9f9481f9b582c5084bb6
1178615156 <a1178615156@gmail.com>
2015-10-16 21:25:09 +0800

    template params

---
+++ module/macros/src/main/scala/macross/TemplateParams.scala
@@ -0,0 +1,34 @@
+package macross
+
+import scala.reflect.macros.blackbox.Context
+import scala.language.experimental.macros
+import scala.annotation.{compileTimeOnly, StaticAnnotation}
+
+/**
+ * Created by YuJieShui on 2015/10/11.
+ */
+
+abstract class Api[T] {
+  def a: Int
+
+  def b: Int
+
+  def t: List[T]
+}
+
+object TemplateParamsMacros {
+  def apply[T](initValue: T): Api[T] = macro impl[T]
+
+  def impl[T: c.WeakTypeTag](c: Context)(initValue: c.Expr[T]): c.Tree = {
+    import c.universe._
+    val t: c.Type = c.weakTypeOf[T]//.typeSymbol.name.toTypeName
+    q"""
+      val api = new Api[$t] {
+       def a = 1
+       def b = 2
+       override def t= List(${initValue})
+     }
+     api
+    """
+  }
+}
