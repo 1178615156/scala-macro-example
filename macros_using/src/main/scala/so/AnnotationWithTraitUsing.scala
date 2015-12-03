ca78a60a270f2c3912c72c461d1c73409d74a10a
1178615156 <a1178615156@gmail.com>
2015-12-03 20:22:58 +0800

    so annotation with trait

--- macros_using/src/main/scala/so/AnnotationWithTraitUsing.scala
+++ macros_using/src/main/scala/so/AnnotationWithTraitUsing.scala
@@ -4,19 +4,24 @@ package so
   * Created by yjs on 2015/12/2.
   */
 
-trait MyTrait[MT] {
+    trait MyTrait[MT1] {
 
-  def x(t1: MT)(t2: MT): MT = t1
+      def x(t1: MT1)(t2: MT1): MT1 = t1
 
-}
+    }
+
+    trait MyTrait2[MT2] {
+      def t(t2: MT2): MT2 = t2
+    }
 
 
-@AnnotationWithTrait
-class MyClass[T] extends MyTrait[T]
+    @AnnotationWithTrait
+    class MyClass[MCT1, MCT2] extends MyTrait[MCT1] with MyTrait2[MCT2]
 
-object AnnotationWithTraitUsing extends App {
-  assert(new MyClass[Int].impl.x(1)(2) == 1)
-}
+    object AnnotationWithTraitUsing extends App {
+      assert(new MyClass[Int, String].x(1)(2) == 1)
+      assert(new MyClass[Int, String].t("aaa") == "aaa")
+    }
 
 
 
--- stackoverflow/src/main/scala/so/AnnotationWithTrait.scala
+++ stackoverflow/src/main/scala/so/AnnotationWithTrait.scala
@@ -1,104 +1,99 @@
-package so
+    package so
 
-import scala.annotation.StaticAnnotation
-import scala.language.experimental.macros
-import scala.reflect.macros.blackbox.Context
+    import scala.annotation.StaticAnnotation
+    import scala.language.experimental.macros
+    import scala.reflect.macros.blackbox.Context
 
-/**
-  * Created by yjs on 2015/12/2.
-  */
-class AnnotationWithTrait extends StaticAnnotation {
-  def macroTransform(annottees: Any*): Any = macro AnnotationWithTraitImpl.apply
+    /**
+      * Created by yu jie shui on 2015/12/2.
+      */
 
-}
+    class AnnotationWithTrait extends StaticAnnotation {
+      def macroTransform(annottees: Any*): Any = macro AnnotationWithTraitImpl.apply
 
-class AnnotationWithTraitImpl(val c: Context) {
-
-  import c.universe._
-
-  val SDKClasses = Set("java.lang.Object", "scala.Any")
-
-  def showInfo(s: String) = c.info(c.enclosingPosition, s.split("\n").mkString("\n |---macro info---\n |", "\n |", ""), true)
-
-  def apply(annottees: c.Expr[Any]*) = {
-    val classDef = annottees.map(_.tree).head.asInstanceOf[ClassDef]
-    val superClass = c.typecheck(classDef).symbol.asClass.baseClasses.tail
-      .filterNot(e => SDKClasses.contains(e.fullName))
-
-    showInfo(show(superClass.map(_.asClass.typeParams.map(_.name))))
-    val superClassTree: List[Tree] = classDef match {
-      case q"$mod class $name[..$t](..$params) extends ..$superClass { ..$body }" =>
-        (superClass: List[Tree]).filterNot(e =>
-          typeOf[Object].members.exists(_.name == e.children.head.toString())
-        )
     }
-    val impl = q"object ${TermName("impl")} extends ..${superClassTree}"
-    //    val method = c.typecheck(classDef).symbol.asClass.info.members
-    //      .filterNot(_.isConstructor)
-    //      .filterNot(e => typeOf[Object].members.exists(_.name == e.name)).map(_.asMethod)
-    //
-    case class TypeParamsTran(from: String, to: String)
-    val method = superClass.flatMap(_.info.members
-      .filterNot(_.isConstructor)
-      .filterNot(e => typeOf[Object].members.exists(_.name == e.name)).map(_.asMethod))
 
-    val o = superClassTree zip superClass map {
-      case (superClassTree, superClass) =>
-          superClass.asClass.typeParams.map(_.name) zip superClassTree.children.tail map
-            (e => TypeParamsTran(e._1.toString, e._2.toString()))
+    class AnnotationWithTraitImpl(val c: Context) {
 
+      import c.universe._
 
-      //        val func: List[c.universe.MethodSymbol] = superClass.info.members
-      //          .filterNot(_.isConstructor)
-      //          .filterNot(e => typeOf[Object].members.exists(_.name == e.name)).map(_.asMethod).toList
-      //        typeParams zip func
-    }
-    showInfo(show(o))
+      val SDKClasses = Set("java.lang.Object", "scala.Any")
 
-    //    map {
-    //      case (typeParams: List[(c.universe.Tree, c.universe.Type)], func: List[c.universe.MethodSymbol]) =>
-    //        func map { e =>
-    //          val funcName = e.name
-    //          val funcTypeParams = e.typeParams.map(_.name.toString).map(name => {
-    //            TypeDef(Modifiers(Flag.PARAM), TypeName(name), List(), TypeBoundsTree(EmptyTree, EmptyTree))
-    //          })
-    //        }
-    //
-    //    }
-    //    showInfo(show(impl))
-    //    showInfo(show(method))
-    val methodImpl =
-      method map (e => {
-        val funcName = e.name
-        val funcTypeParams = e.typeParams.map(_.name.toString).map(name => {
-          TypeDef(Modifiers(Flag.PARAM), TypeName(name), List(), TypeBoundsTree(EmptyTree, EmptyTree))
-        })
-        val funcParams = e.paramLists.map(_.map(e => q"${e.name.toTermName}:${e.info} "))
+      def showInfo(s: String) = c.info(c.enclosingPosition, s.split("\n").mkString("\n |---macro info---\n |", "\n |", ""), true)
+
+      def apply(annottees: c.Expr[Any]*) = {
+
+        val classDef = annottees.map(_.tree).head.asInstanceOf[ClassDef]
+
+        val superClassSymbol= c.typecheck(classDef).symbol.asClass.baseClasses.tail
+          .filterNot(e => SDKClasses.contains(e.fullName)).reverse
+
+        val superClassTree= classDef match {
+          case q"$mod class $name[..$t](..$params) extends ..$superClass { ..$body }" =>
+            (superClass: List[Tree]).filterNot(e =>
+              typeOf[Object].members.exists(_.name == e.children.head.toString())
+            )
+        }
+
+        showInfo(show(superClassSymbol))
+        showInfo(show(superClassTree))
+
+        val impl = q"private[this] object ${TermName("impl")} extends ..${superClassTree}"
+        //
+
+        //get super class all can call method
+        val methods = superClassSymbol.map(_.info.members
+          .filterNot(_.isConstructor)
+          .filterNot(e => typeOf[Object].members.exists(_.name == e.name)).map(_.asMethod)).toList
+
+        case class ReplaceTypeParams(from: String, to: String)
+        type ClassReplace = List[ReplaceTypeParams]
+
+        //trait a[A]
+        //class b[B] extends a[B]
+        //need replace type params A to B
+        val classReplaceList: List[ClassReplace] = superClassTree zip superClassSymbol map {
+          case (superClassTree, superClassSymbol) =>
+            superClassSymbol.asClass.typeParams.map(_.name) zip superClassTree.children.tail map
+              (e => ReplaceTypeParams(e._1.toString, e._2.toString()))
+        }
+
+        val out = classReplaceList zip methods map {
+          case (classReplace, func) =>
+
+            func map { e => {
+
+              val funcName = e.name
+
+              val funcTypeParams = e.typeParams.map(_.name.toString).map(name => {
+                TypeDef(Modifiers(Flag.PARAM), TypeName(name), List(), TypeBoundsTree(EmptyTree, EmptyTree))
+              })
+
+              val funcParams = e.paramLists.map(_.map(e => q"${e.name.toTermName}:${
+                TypeName(
+                  classReplace.find(_.from == e.info.toString).map(_.to).getOrElse(e.info.toString)
+                )} "))
+
+              val funcResultType = TypeName(
+                classReplace.find(_.from == e.returnType.toString).map(_.to).getOrElse(e.info.toString)
+              )
+              q"""
+               def ${funcName}[..${funcTypeParams}](...$funcParams):${funcResultType}=
+                  impl.${funcName}[..${funcTypeParams}](...$funcParams)
+                """
+            }
+            }
+
+        }
+
+        showInfo(show(out))
+
         q"""
-           def ${funcName}[..${funcTypeParams}](...$funcParams):
-        ${e.returnType}=
-        impl.${funcName}[..${funcTypeParams}](...$funcParams)
-            """
+           class ${classDef.name}[..${classDef.tparams}]{
+            $impl
+            ..${out.flatten}
+           }
+          """
       }
-        )
-    showInfo(show(methodImpl))
-    //    import Flag._
-    //    DefDef(Modifiers(DEFERRED), TermName("f"), List(),
-    //      List(List(ValDef(Modifiers(PARAM), TermName("a"), Ident(TypeName("Int")), EmptyTree), ValDef(Modifiers(PARAM), TermName("b"), Ident(
-    //      TypeName("String")), EmptyTree)), List(ValDef(Modifiers(PARAM), TermName("c"), Ident(TypeName("Any")), EmptyTree))), Select(Ident("scala"), TypeName("Unit")), EmptyTree)
-    //
-    //    showInfo(showRaw(q"def f(a:Int,b:String)(c:Any)"))
-    q"""
-       class ${classDef.name}[..${classDef.tparams}]{
-        $impl
-..${methodImpl}
-       }
-      """
-  }
-}
+    }
 
-//        ..${method.map(e=>q"""
-//def ${e.name}[..${e.typeParams.map(_.name)}](...${e.paramLists.map(_.map(_.name))}) =
-//impl.${e.name}[..${e.typeParams.map(_.name)}](...${e.paramLists.map(_.map(_.name))})
-//
-//                      """)}
