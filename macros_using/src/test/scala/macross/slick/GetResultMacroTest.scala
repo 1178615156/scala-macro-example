76ef198fcc908d527ce76f4da2999404b7efdd1c
117861516 <a1178615156@gmail.com>
2016-01-26 21:16:43 +0800

    slick GetResultMacro and test

---
+++ macros_using/src/test/scala/macross/slick/GetResultMacroTest.scala
@@ -0,0 +1,26 @@
+package macross.slick
+
+import slick.jdbc.{PositionedResult, GetResult}
+import slick.lifted.ProvenShape
+
+/**
+  * Created by yuJieShui on 2016/1/26.
+  */
+class GetResultMacroTest {
+
+  import slick.driver.MySQLDriver.api._
+
+  case class Address(street: String, city: String, code: Option[Int])
+
+  case class User(id: Long, name: String, age: Option[Int], address: Address, address2: Option[Address])
+
+  implicit val implicitGetResultAddress = GetResultMacro.apply[Address]
+
+  implicit val implicitGetResultUser =
+    GetResult(e⇒new User(e.<<?))
+    GetResultMacro.apply[User]
+
+
+  sql"SELECT * FROM User WHEN TRUE ".as[User]
+
+}
