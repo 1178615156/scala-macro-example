3952ca30ae02674a7920969a19e8ec15529be145
1178615156 <a1178615156@gmail.com>
2016-03-15 10:12:25 +0800

    Update BuildSettings.scala

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -36,9 +36,7 @@ object BuildSettings {
   val infoSetting = Seq(
     version := "1.0-SNAPSHOT",
     scalaVersion := "2.11.6",
-    //    organization := "com.yjs"
-    //    organization := "com"
-    organization := "com.wecash.financal"
+    organization := "com.yujieshui"
 
   )
 
