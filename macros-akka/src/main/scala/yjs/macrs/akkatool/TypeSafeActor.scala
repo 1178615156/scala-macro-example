3df73d0f54d0fc35fc5be7ac468af8d992a57926
yujieshui <a1178615156@gmail.com>
2016-11-08 22:35:32 +0800

    add type safe actor receive

--- macros-akka/src/main/scala/yjs/macrs/akkatool/TypeSafeActor.scala
+++ macros-akka/src/main/scala/yjs/macrs/akkatool/TypeSafeActor.scala
@@ -3,19 +3,23 @@ package yjs.macrs.akkatool
 import akka.actor.ActorRef
 import yjs.macrs.common.TypeUtils
 
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 import scala.language.experimental.macros
 import scala.reflect.macros.blackbox
 
 /**
   * Created by yujieshui on 2016/11/5.
   */
-object FromActor{
-  @deprecated("use TypeSafeActor","")
+@deprecated("use TypeSafeActor", "")
+object FromActor {
+  @deprecated("use TypeSafeActor", "")
   def apply[T](actorRef: ActorRef): T = macro TypeSafeActorImpl.fromActor[T]
 }
+
 object TypeSafeActor {
   def fromActor[T](actorRef: ActorRef): T = macro TypeSafeActorImpl.fromActor[T]
+
+  def receive[Api](api: Api)(implicit ec: ExecutionContext): PartialFunction[Any, Unit] = macro TypeSafeActorImpl.receive[Api]
 }
 
 class TypeSafeActorImpl(override val c: blackbox.Context) extends TypeUtils {
@@ -25,12 +29,40 @@ class TypeSafeActorImpl(override val c: blackbox.Context) extends TypeUtils {
   def getAbstractMethod(tye: Type) =
     tye.members.filter(_.isAbstract).filter(_.isMethod).map(_.asMethod)
 
+  def requireMethod(method: MethodSymbol) =
+    if(method.paramLists.size != 1 || method.paramLists.head.size != 1) c.abort(c.enclosingPosition, "method params must as 1 ")
+
+  //
+  def receive[Api: c.WeakTypeTag](api: c.Expr[Api])(ec: c.Expr[ExecutionContext]): c.Expr[PartialFunction[Any, Unit]] = {
+    val t = c.weakTypeOf[Api]
+
+    val abstractMethods = getAbstractMethod(t)
+    val outMethods: Iterable[c.universe.Tree] = abstractMethods.map { method =>
+      requireMethod(method)
+      val param = method.paramLists.head.head
+      val paramType = param.typeSignature
+      val returnType = method.returnType
+
+      returnType match {
+        case e if asUnit(e)   => cq" x:$paramType => ${api}.${method.name}(x)"
+        case e if asFuture(e) => cq" x:$paramType => akka.pattern.pipe(${api}.${method.name}(x)).pipeTo(sender())"
+        case e                => c.abort(c.enclosingPosition, "method return type is not [unit or future]")
+      }
+    }
+    val out = q"PartialFunction[Any,Unit]{case ..$outMethods}"
+
+    c.echo(c.enclosingPosition, out.toString())
+    c.Expr[PartialFunction[Any, Unit]](out)
+  }
+
+
+  //
   def fromActor[T: c.WeakTypeTag](actorRef: c.Expr[ActorRef]): c.Expr[T] = {
     val t = c.weakTypeOf[T]
     val abstractMethods = getAbstractMethod(t)
 
     val outMethods = abstractMethods.map(method => {
-      if(method.paramLists.size != 1 || method.paramLists.head.size != 1) c.abort(c.enclosingPosition, "method params must as 1 ")
+      requireMethod(method)
 
       val param = method.paramLists.head.head
       val returnType = method.returnType
@@ -45,14 +77,14 @@ class TypeSafeActorImpl(override val c: blackbox.Context) extends TypeUtils {
 
       q"override def ${method.name}(...${params}):${returnType}= {$body} "
     })
-    c.echo(c.enclosingPosition, outMethods.map(e => show(e)).mkString("\n", "\n", ""))
-    c.Expr[T](
+    val out =
       q"""
        new $t {
         ..${outMethods}
        }
       """
-    )
+    c.echo(c.enclosingPosition,show(out))
+    c.Expr[T](out)
   }
 
 }
--- macros-test/src/test/scala/yjs/macrs/akkatool/FromActorTest.scala
+++ macros-test/src/test/scala/yjs/macrs/akkatool/FromActorTest.scala
@@ -1,29 +1,39 @@
 package yjs.macrs.akkatool
 
-import akka.actor.{ActorRef, ActorSystem}
+import akka.actor.Actor.Receive
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
 import org.scalatest.{BeforeAndAfterAll, Suite, WordSpecLike}
 import akka.pattern.ask
 import akka.testkit.TestKit
 import akka.util.Timeout
-import yjs.macrs.akkatool.Api.{Hello, HelloResult}
+import com.sun.rowset.internal.InsertRow
 
-import scala.concurrent.Future
+import scala.concurrent.{Await, Future}
 import scala.concurrent.duration._
 
 /**
   * Created by yujieshui on 2016/11/5.
   */
 trait Api {
+
+  import Api._
+
+  def send(s: String): Unit
+
   def ask(i: Int): Future[String]
 
   def ask(hello: Hello): Future[HelloResult]
 
-  def send(s: String): Unit
-
   def hello = ask(Hello())
+
+  def insert(i: Insert): Future[InsetResult]
 }
 
 object Api {
+
+  case class Insert(i: Int)
+
+  case class InsetResult(result: Boolean)
 
   case class Hello()
 
@@ -36,6 +46,26 @@ object Api {
   def fromActor(actorRef: ActorRef): Api = TypeSafeActor.fromActor[Api](actorRef)
 }
 
+import Api._
+
+class ApiActor extends Actor with Api {
+
+  import context.dispatcher
+
+  override def ask(i: Int): Future[String] = {
+    Future.successful(i.toString)
+  }
+
+  override def ask(hello: Hello): Future[HelloResult] = Future.successful(HelloResult())
+
+  override def send(s: String): Unit = ()
+
+  override def insert(i: Insert): Future[InsetResult] = Future.successful(InsetResult(true))
+
+  override def receive: Receive = TypeSafeActor.receive[Api](this)
+}
+
+
 trait StopSystemAfterAll extends BeforeAndAfterAll {
   this: Suite with TestKit =>
   override protected def afterAll(): Unit = {
@@ -44,12 +74,12 @@ trait StopSystemAfterAll extends BeforeAndAfterAll {
 
 }
 
-class FromActorTest
+class TypeSafeActorTest
   extends TestKit(ActorSystem("test"))
     with WordSpecLike
     with StopSystemAfterAll {
 
-  "api" must {
+  "fromActor" must {
     val api = Api.fromActor(testActor)
     "ask" in {
       api.ask(1)
@@ -66,6 +96,18 @@ class FromActorTest
       expectMsg(Hello())
     }
 
+  }
+
+
+  "receive" must {
+    val actor = system.actorOf(Props(new ApiActor))
+    val api = Api.fromActor(actor)
+    "ask" in {
+      assert(Await.result(api.ask(1), Duration.Inf) === "1")
+      assert(Await.result(api.ask(2), Duration.Inf) === "2")
+      assert(Await.result(api.ask(Hello()), Duration.Inf) === HelloResult())
+      assert(Await.result(api.insert(Insert(11)), Duration.Inf) === InsetResult(true))
+    }
   }
 
 
